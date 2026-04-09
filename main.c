526dc68bb8966828cabeb44b90770aad17b32f4c
Leonardo <jrleo010906@gmail.com>
2026-04-09 20:54:18 -0300

    adicionado a movimentação das peças de acordo como pedido na aula

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+    int torre = 0;
+    int bispo = 0;
+    int rainha= 0;
+    //movimentando a torre
+    for(torre ; torre < 5 ; torre++){
+        printf("torre\n");
+        printf("direita\n");
+    }
+    printf("\n");
+    //movimentando o bispo
+
+    while(bispo < 5){
+        bispo++;
+        printf("bispo\n");
+        printf("cima,direita\n");
+
+    }
+        printf("\n");
+    //movimentando a Rainha
+    do{
+        rainha++;
+        printf("rainha\n");
+        printf("esquerda\n");
+    }while(rainha < 8);
+       return 0;
 }
+
